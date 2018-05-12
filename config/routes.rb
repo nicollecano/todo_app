e022c03559233c83f2eb840584ef4271b0fa1600
Nicolle Cano <nicollecano291@gmail.com>
2018-05-12 16:03:24 +0000

    id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
  
  get 'todo/index', to: 'todo#index'
- get 'todo/show', to: 'todo#show'
- get 'todo/show2', to: 'todo#show2'
- get 'todo/show3', to: 'todo#show3'
-
+ get 'todo/show/:id', to: 'todo#show'
+ 
  end
