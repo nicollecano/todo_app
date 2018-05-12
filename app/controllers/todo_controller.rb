990f9730702664392becb4226e0f82b37447967b
Nicolle Cano <nicollecano291@gmail.com>
2018-05-12 18:04:47 +0000

    template

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,20 @@
 class TodoController < ApplicationController
  def index
- @todo_description = "Make the curriculum"
- @todo_pomodoro_estimate = 4
- @arize_description = 'awesome'
  end
-end 
+ def show
+    todo_id=params[:id]
+    if todo_id == '1'
+      @todo_1 = "Change my bed sheets"
+      @todo_2 = "Sweep and mop floor"
+      @todo_3 = "Fix my closet"
+    elsif todo_id == '2'
+      @todo_1 = "Walk to nearest train station"
+      @todo_2 = "Take the E train"
+      @todo_3 = "Get out of train station"
+    else
+      @todo_1 = "Get materials together"
+      @todo_2 = "Look up homework"
+      @todo_3 = "Check all homework was completed"
+    end
+ end
+end
