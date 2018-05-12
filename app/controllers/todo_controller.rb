0ae2775cc591630c177d34dc6d6d2069e1b3afc6
Nicolle Cano <nicollecano291@gmail.com>
2018-05-12 15:36:54 +0000

    variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,10 +1,7 @@
 class TodoController < ApplicationController
  def index
+ @todo_description = "Make the curriculum"
+ @todo_pomodoro_estimate = 4
+ @arize_description = 'awesome'
  end
- 
-    
-    
-    
-    
-    
 end 
